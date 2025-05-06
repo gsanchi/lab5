7da21a1a96fba80e355f0eff1e91266fe8946bfb
gsanchi <g20fifi@gmail.com>
2025-05-05 21:46:07 -0600

    segundo push

--- src/ALU.vhd
+++ src/ALU.vhd
@@ -59,26 +59,47 @@ architecture Behavioral of ALU is
             o_flags : in std_logic 
         );
     end component;
-     
-    component full_adder is
-        port (
-            A     : in std_logic;
-            B     : in std_logic;
-            Cin   : in std_logic;
-            S     : out std_logic;
-            Cout  : out std_logic
-        );
-        end component full_adder;
-             
-    signal w_flags : std_logic_vector(2 downto 0);
-    signal w_result : std_logic_vector(2 downto 0);
+            
+    signal w_carry  : STD_LOGIC_VECTOR(2 downto 0);
+    signal w_B  : STD_LOGIC_VECTOR(7 downto 0);
+    signal w_sum  : STD_LOGIC_VECTOR(7 downto 0);
+    signal w_and  : STD_LOGIC_VECTOR(7 downto 0);
+    signal w_or  : STD_LOGIC_VECTOR(7 downto 0);
+    signal w_flags : std_logic_vector(3 downto 0);
+    signal w_result : std_logic_vector(3 downto 0);
     
 begin
-    complete_ALU_0 : complete_ALU
-    port map(
-        i_A     => A(0);
+    w_B <= not i_B when i_op ="000" 
+    else i_B;
     
+    complete_ALU_0: ripple_adder
+    port map(
+        A => i_A(3 downto 0),
+        B => w_B(3 downto 0),
+        Cin => i_op(0),
+        S => w_sum(3 downto 0),
+        Cout => w_carry(0)
     );
 
+    complete_ALU_1: ripple_adder
+    port map(
+        A => i_A(7 downto 4),
+        B => w_B(7 downto 4),
+        Cin => w_carry(0),
+        S => w_sum(7 downto 4),
+        Cout => w_carry(1)
+    );
+    
+    w_result <= w_sum(7 downto 0) when i_op = "000" else
+                w_sum(7 downto 0) when i_op = "001" else
+                (i_A and i_B) when i_op = "010" else
+                (i_A or i_B) when i_op = "011" else
+                (others => '0');
+    o_flags(3) <= w_sum(7);
+    o_flags(2) <= '1' when (w_result = "00000000")
+                   else '0';
+    o_flags(1) <= w_carry(1) and not(i_op(1));
+    o_flags(0) <= not(i_A(7) xor i_B(7) xor i_op(0)) and (i_A(7) xor w_result(7)) and (not i_op(1));
+    o_result <= w_result;
 
 end Behavioral;
